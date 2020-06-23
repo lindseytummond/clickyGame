ba8014b36c80204bafefdf6e2f835ec5252a7a4e
lindsey tummond <lindseytummond@gmail.com>
2020-06-22 18:06:43 -0700

    Updates

---
+++ precache-manifest.386d2d0c27f6ea6e042b77dcaeac51aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4884b0601decd567d0a603c5cd3e10",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "329455e1a747039c4021",
+    "url": "/clickyGame/static/css/main.4a5da65a.chunk.css"
+  },
+  {
+    "revision": "aaeac0538460f85b3011",
+    "url": "/clickyGame/static/js/2.93fe0e83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickyGame/static/js/2.93fe0e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329455e1a747039c4021",
+    "url": "/clickyGame/static/js/main.04148997.chunk.js"
+  },
+  {
+    "revision": "6a7b678e941b9efa1ad9",
+    "url": "/clickyGame/static/js/runtime-main.13578205.js"
+  }
+]);
